ec4ceb5db5526b59777e0bbf12536e0899b3f5b3
Tanishq Kr. Kaushal <90266615+tanishqkrk@users.noreply.github.com>
2022-09-24 21:58:48 +0530

    cleaner version

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-n = float(input("Enter a decimal number: "))
-x = len(str(n))
-
-
-def part(n):
+def decsep(n):
     temp = 0
+    x = len(str(n))
     a = int(n)
-
     ba = n % 1
     b_round = round(ba, x)
     b_temp = str(b_round)
     for i in b_temp:
         b_slice = b_temp[2::]
     b = b_slice
+    x = int(b)
 
-    print(a)
-    print(b)
+    return [a, x]
 
-
-part(n)
+print(decsep(4.54))
